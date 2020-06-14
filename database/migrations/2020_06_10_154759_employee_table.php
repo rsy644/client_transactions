f2dddcf1e1e12f4036bce4de140c601bd086bb59
Robert Young <robert@young1.org>
2020-06-14 21:13:05 +0100

    Changed foreign key constraint

--- database/migrations/2020_06_10_154759_employee_table.php
+++ database/migrations/2020_06_10_154759_employee_table.php
@@ -17,7 +17,7 @@ class EmployeeTable extends Migration
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
-            $table->integer('company_id');
+            $table->integer('company_id')->unsigned();
             $table->foreign('company_id')->references('id')->on('companies');
             $table->string('email');
             $table->string('phone');
