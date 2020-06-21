b5a4e09402a396f55487df66740d4a5e02559ca4
Robert Young <robert@young1.org>
2020-06-21 18:10:18 +0100

    Installed dusk and wrote some dusk tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 
 use Illuminate\Support\Facades\Schema;
 
+use Laravel\Dusk\DuskServiceProvider;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -15,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+         if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+         }
     }
 
     /**
